ca972678bc4fdf7d5930cd38638d482e42d2f7c6
Mahfuz Ayon <134989125+Mahfuz-Ayon73@users.noreply.github.com>
2025-05-15 15:37:06 +0600

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express'
 import dotenv from 'dotenv'
 import connectToMongoDB from './db/connetToMongoDB.js'
@@ -9,18 +10,22 @@ import { app , server} from './socket/socket.js'
 
 dotenv.config()
 
+const __dirname = path.resolve();
 // const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/',(req , res) => {
-    res.send('Hello from Server')
-})
 
 app.use(express.json()) // middleware to extract data from res.body
 app.use(cookieParser())
 app.use('/api/auth',authRoutes) // middleware
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT , () => {
     connectToMongoDB()
